6fa5bc28b3b80c0720ca5a6d2fd24a48b770aff1
superxmanyang <superxmanyang@163.com>
2020-01-10 11:42:38 +0800

    添加时间戳

--- kit.js
+++ kit.js
@@ -1,6 +1,15 @@
 //这里面封装我们的常用代码的jS代码
 var kits = {};
 
+
+// 多少到多少之间的随机数字
 kits.randonInt = function(n, m) {
   return Math.floor(Math.random() * (m - n + 1) + n);
 };
+
+
+
+// 十位数字的时间戳
+kits.timestamp = function(){
+  return Date.parse(new Date()) / 1000;
+}
