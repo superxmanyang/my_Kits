4d7382a7b3d552352dc54bdeb6d653a0f758832d
JackyMao <superxmanyang@163.comommmmm>
2020-03-12 18:37:55 +0800

    更新

--- kit.js
+++ kit.js
@@ -3,13 +3,25 @@ var kits = {};
 
 
 // 多少到多少之间的随机数字
-kits.randonInt = function(n, m) {
+kits.randonInt = function (n, m) {
   return Math.floor(Math.random() * (m - n + 1) + n);
 };
 
 
 
 // 十位数字的时间戳
-kits.timestamp = function(){
+kits.timestamp = function () {
   return Date.parse(new Date()) / 1000;
+}
+
+// 去重
+kits.quchong = function (arr) {
+  var newArr = []
+  for (var i = 0; i < arr.length; i++) {
+    if (newArr.indexOf(arr[i]) === -1) {
+      newArr.push(arr[i]);
+    }
+  }
+
+  return newArr
 }
